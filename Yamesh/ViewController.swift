0beb5e2cd886c3d8c19495a5d89592e12dfad26e
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2016-10-25 17:44:32 +0900

    LEDペリフェラルのスキャン、LEDの点灯をボタンからできるようにした

--- Yamesh/ViewController.swift
+++ Yamesh/ViewController.swift
@@ -10,19 +10,67 @@ import UIKit
 import CoreBluetooth
 
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    let noPeripheralLabel = "(no peripheral)"
+    let scanningLabel = "Scanning..."
+
     var centralManager: CBCentralManager?
     var peripheral: CBPeripheral?
+    var ledPeripheral: CBPeripheral?
+
+    @IBOutlet weak var peripheralNameLabel: UILabel!
+    @IBOutlet weak var scanButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         centralManager = CBCentralManager(delegate: self, queue: nil)
+
+        initialize()
+    }
+
+    func initialize() {
+        // ペリフェラルのクリア
+        self.ledPeripheral = nil
+        self.peripheralNameLabel.text = noPeripheralLabel
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // MARK - Actions
+
+    @IBAction func scanButtonDidPress(_ sender: AnyObject) {
+
+        if scanButton.currentTitle == "SCAN" {
+            // ペリフェラルのクリア
+            self.ledPeripheral = nil
+            self.peripheralNameLabel.text = scanningLabel
+
+            // LEDサービスのスキャンを開始
+            // serviceのUUID: 72C90001-57A9-4D40-B746-534E22EC9F9E
+            let service = CBUUID(string: "19B10000-E8F2-537E-4F6C-D104768A1214")
+            self.centralManager?.scanForPeripherals(withServices: [service], options: nil)
+
+            // スキャンボタンのラベル更新
+            scanButton.setTitle("STOP SCAN", for: UIControlState.normal)
+        } else {
+            self.centralManager?.stopScan()
+            self.peripheralNameLabel.text = noPeripheralLabel
+            scanButton.setTitle("SCAN", for: UIControlState.normal)
+        }
+
+    }
+
+    // LEDを点灯する
+    @IBAction func onButtonDidPress(_ sender: AnyObject) {
+        if let peripheral = self.ledPeripheral {
+
+            // 接続開始
+            self.centralManager?.connect(peripheral, options: nil)
+        }
     }
 
     // MARK: - CBCentralManagerDelegate
@@ -33,13 +81,6 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
             print("state: poweredOff")
         case .poweredOn:
             print("state: poweredOn")
-
-            // スキャンを開始
-            self.centralManager?.scanForPeripherals(withServices: nil, options: nil)
-
-            // LEDサービス（ serviceのUUID = 72C90001-57A9-4D40-B746-534E22EC9F9E ）のスキャンを開始
-            let service = CBUUID(string: "19B10000-E8F2-537E-4F6C-D104768A1214")
-            self.centralManager?.scanForPeripherals(withServices: [service], options: nil)
         case .resetting:
             print("state: resetting")
         case .unauthorized:
@@ -54,11 +95,14 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         print("発見したBLEデバイス: \(peripheral)")
 
-        // ペリフェラルがメモリから開放されないよう、インスタンス変数へ保持しておく
-        self.peripheral = peripheral
+        // LEDペリフェラルが見つかったので保持しておく
+        self.ledPeripheral = peripheral
 
-        // 接続開始
-        self.centralManager?.connect(peripheral, options: nil)
+        // ペリフェラル名を表示
+        self.peripheralNameLabel.text = peripheral.name
+
+        // スキャンボタンのラベルを戻す
+        self.scanButton.setTitle("SCAN", for: UIControlState.normal)
     }
 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
@@ -76,33 +120,22 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
 
         if let service = peripheral.services?.first {
             // キャラクタリスティックを探索
-//            let characteristic = CBUUID(string: "19B10001-E8F2-537E-4F6C-D104768A1214")
             peripheral.discoverCharacteristics(nil, for: service)
         }
     }
 
+    // XXX: 今はLED点灯に特化しちゃってるので、徐々に汎化していく予定
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         if let characteristics = service.characteristics {
             print("\(characteristics.count) 個のキャラクタリスティックを発見")
 
             if let characteristic = characteristics.first {
-                // キャラクタリスティックからのRead
-                peripheral.readValue(for: characteristic)
+                // 書き込みを行う
+                let newValue: UInt8 = 1
+                let newData = NSData(bytes: [newValue], length: 1)
+                peripheral.writeValue(newData as Data, for: characteristic, type: CBCharacteristicWriteType.withResponse)
             }
         }
-    }
-
-    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        print("読み込み成功！ service: \(characteristic.service.uuid), characteristic: \(characteristic.uuid)")
-
-        if let data = characteristic.value {
-            let s = data.map { String(format: "%02X", $0) }.joined()
-            print("value: \(s)")
-        }
-
-        // 書き込みを行う
-        let newValue = NSData(bytes: [0x1], length: 1)
-        peripheral.writeValue(newValue as Data, for: characteristic, type: CBCharacteristicWriteType.withResponse)
     }
 
     func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
